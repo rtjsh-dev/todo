e761275c56400bace347b6f56cbd07053925b5fe
rtjsh-dev <rajesh025thapa@gmail.com>
2026-01-31 23:04:33 +0545

    add task completed

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Task
 
 # Create your views here.
 def addTask(request):
-    HttpResponse("The form is submitted.")
+    # print(request.POST['task'])
+    task = request.POST['task']
+    Task.objects.create(task=task) 
+    # Only variable value here is 'task', so, we will be using that value only
+    return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from ToDo.models import Task
 
 def home(request):
-    tasks = Task.objects.filter(is_Completed=False) 
+    tasks = Task.objects.filter(is_Completed=False).order_by('updated_at')
     # print(list(tasks))
     context = {
         'tasks': tasks,
