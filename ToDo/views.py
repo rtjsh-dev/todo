3c6163461f85fdadf253b2ef6cddee8e32b438b4
rtjsh-dev <rajesh025thapa@gmail.com>
2026-02-01 11:41:52 +0545

    mark as done completeed

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
 # Create your views here.
@@ -8,4 +8,11 @@ def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task) 
     # Only variable value here is 'task', so, we will be using that value only
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    # print(pk)
+    task = get_object_or_404(Task, pk=pk)
+    task.is_Completed =True
+    task.save()
     return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -3,8 +3,11 @@ from ToDo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_Completed=False).order_by('updated_at')
+    completed_tasks = Task.objects.filter(is_Completed=True)
+    # print(completed_tasks)
     # print(list(tasks))
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
         }
-    return render(request, 'home.html', context)
+    return render(request, 'home.html', context, )
