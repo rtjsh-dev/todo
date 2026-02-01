8e48df6db16b4e53cd9ddc4dec7d40d5c6090277
rtjsh-dev <rajesh025thapa@gmail.com>
2026-02-01 21:20:39 +0545

    functionality added

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
@@ -11,8 +10,31 @@ def addTask(request):
     return redirect('home')
 
 def mark_as_done(request, pk):
-    # print(pk)
     task = get_object_or_404(Task, pk=pk)
-    task.is_Completed =True
-    task.save()
+    task.is_Completed = True
+    task.save(update_fields=["is_Completed"])
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_Completed = False
+    task.save(update_fields=["is_Completed"])
+    return redirect('home')
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        new_task = request.POST['task']
+        task.task = new_task
+        task.save()
+        return redirect('home')
+    else:
+        context ={
+            'task':task
+            }
+        return render(request, 'edit_task.html', context)
+    
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect('home')
